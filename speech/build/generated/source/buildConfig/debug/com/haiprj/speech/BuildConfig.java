a8671255b673ff50a30b16a79d09a6b8367e9c36
@tranhai16 <ripallrank@gmail.com>
2024-08-12 20:30:21 +0700

    update 3

---
+++ speech/build/generated/source/buildConfig/debug/com/haiprj/speech/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.haiprj.speech;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.haiprj.speech";
+  public static final String BUILD_TYPE = "debug";
+}
