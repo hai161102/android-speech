b33c7fbaa811c1a579076566fbf6a25ef6844a16
@tranhai16 <ripallrank@gmail.com>
2024-08-12 23:46:55 +0700

    update master

--- app/src/main/java/com/hprj/speechrecognizer/SupportedLanguagesListener.java
+++
@@ -1,8 +0,0 @@
-package com.hprj.speechrecognizer;
-
-import java.util.List;
-
-public interface SupportedLanguagesListener {
-    void onSupportedLanguages(List<String> supportedLanguages);
-    void onNotSupported(UnsupportedReason reason);
-}
